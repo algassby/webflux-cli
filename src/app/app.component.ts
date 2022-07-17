36fedee5827672da3edae63e8ce3586d77ebac40
algassby <algassby@hotmail.com>
2022-07-17 13:30:51 +0200

    config and commit template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { Observable, Observer } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'weblux-cli';
+  endSearch : boolean = false;
+  showProgressbar: boolean = false;
+  results: string [] = [];
+  resultsObserver : Observable<string[]> = new Observable<string[]>();
+
+  public constructor(private ngZone: NgZone){
+
+  }
+
+  search(event: any){
+    console.log(event.target);
+    event.preventDefault();
+    this.showProgressbar = true;
+    this.endSearch = true;
+    this.results = [];
+    this.resultsObserver = this.createEventSourceObserver();
+    console.log(this.resultsObserver);
+  }
+
+  createEventSourceObserver(): Observable<string[]>{
+    console.log("observer");
+    return new Observable<string[]>((observer: Observer<string[]>)=>{
+      const source = new EventSource(environment.apiUrl);
+      source.onmessage = (event)=>{
+        console.log(event.data);
+        this.results.push(event.data);
+        this.results.sort();
+        this.ngZone.run(()=> observer.next(this.results));
+      };
+
+
+      source.onopen = (event)=>{
+        if(this.endSearch){
+          source.close();
+          this.ngZone.run(()=>{
+            observer.complete();
+            this.showProgressbar = false;
+          });
+        }
+        this.endSearch = true;
+      };
+    }
+      
+   );
+
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
